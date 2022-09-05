ea704cd5ba0dc599c245a43c3ca83c1bc1ff9c8c
Usman Tanveer <usman.tanveer@gmail.com>
2022-09-05 15:16:10 +0200

    New features

---
+++ DataTablesNet.Module/Helpers/DTHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataTablesNet.Module.Helpers {
+    public class DTHelper {
+        public DTHelper() {
+        }
+
+        public static IEnumerable<T> Sort<T>(IEnumerable<T> data, DTRequest request) {
+            IOrderedEnumerable<T> sortedData = data.OrderBy(d => 1);
+            foreach (var order in request.Order) {
+                var propertyName = request.Columns[order.Column].Name;
+                if (string.IsNullOrWhiteSpace(propertyName)) {
+                    continue;
+                }
+
+                var property = typeof(T).GetProperty(propertyName);
+                if (property == null) {
+                    continue;
+                }
+
+                if (order.Dir.Equals("asc", StringComparison.InvariantCultureIgnoreCase)) {
+                    sortedData = sortedData.ThenBy(d => property.GetValue(d));
+                }
+                else {
+                    sortedData = sortedData.ThenByDescending(d => property.GetValue(d));
+                }
+            }
+
+            return sortedData;
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> data, DTRequest request, out int recordsFiltered) {
+            IEnumerable<T> filteredData = data;
+            recordsFiltered = filteredData.Count();
+
+            if (string.IsNullOrWhiteSpace(request.Search.Value)) {
+                return data;
+            }
+
+            filteredData = data.Where(d => d.ToString().IndexOf(request.Search.Value) > -1);
+            recordsFiltered = filteredData.Count();
+            return filteredData;
+        }
+
+        public static IEnumerable<T> Page<T>(IEnumerable<T> data, DTRequest request) {
+            return data.Skip(request.Start).Take(request.Length);
+        }
+
+        public static IEnumerable<T> PrepareResult<T>(IEnumerable<T> data, DTRequest request, out int totalRecords, out int recordsFiltered) {
+            totalRecords = data.Count();
+
+            IEnumerable<T> result = data;
+            result = Sort(result, request);
+            result = Filter(result, request, out recordsFiltered);
+            result = Page(result, request);
+
+            return result;
+        }
+    }
+}
+
--- DataTablesNet.Module/Response/DTResponse.cs
+++ DataTablesNet.Module/Response/DTResponse.cs
@@ -1,17 +1,17 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DataTablesNet.Module.Response {
-   public class DTResponse<T> {
-      [JsonProperty("draw")]
-      public int Draw { get; set; }
-      [JsonProperty("recordsTotal")]
-      public int RecordsTotal { get; set; }
-      [JsonProperty("recordsFiltered")]
-      public int RecordsFiltered { get; set; }
-      [JsonProperty("data")]
-      public IEnumerable<T> Data { get; set; }
-      [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
-      public string Error { get; set; }
-   }
+    public class DTResponse<T> {
+        [JsonProperty("draw")]
+        public int Draw { get; set; }
+        [JsonProperty("recordsTotal")]
+        public int RecordsTotal { get; set; }
+        [JsonProperty("recordsFiltered")]
+        public int RecordsFiltered { get; set; }
+        [JsonProperty("data")]
+        public IEnumerable<T> Data { get; set; }
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
 }
